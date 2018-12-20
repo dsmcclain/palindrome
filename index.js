56e603fbad507815cc63aa90a594844ac919ad75
Dylan McClain <dylan.mcclain@gmail.com>
2018-12-20 10:16:53 -0800

    write letters function, test suite passing

--- index.js
+++ index.js
@@ -15,17 +15,18 @@ function Phrase(content) {
 
 	//Returns the letters in the content.
 	this.letters = function letters() {
-		return this.content; //stub return value
+		let theLetters = [];
+		for (let i = 0; i < this.content.length; i++) {
+			if (this.content.charAt(i).match(/[a-zA-Z]/)) {
+				theLetters.push(this.content.charAt(i));
+			}
+		}
+		return theLetters.join("");
 	}
 	
-	//Returns content swithced to lower case
-	this.processor = function(string) {
-		return string.toLowerCase();
-	}
-
 	//Returns content processed for palindrome testing.
 	this.processedContent = function processedContent() {
-		return this.processor(this.content);
+		return this.letters().toLowerCase();
 	}
 
 	//Returns true if the phrase is a palindrome, false otherwise
